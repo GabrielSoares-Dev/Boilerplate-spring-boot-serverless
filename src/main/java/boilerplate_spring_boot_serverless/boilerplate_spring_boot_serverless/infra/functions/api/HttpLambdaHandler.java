1213d3887895ba15b74f3cb06bf093a1fd12de82
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2025-02-12 13:01:02 +0000

    feat: ci pipeline

--- src/main/java/boilerplate_spring_boot_serverless/boilerplate_spring_boot_serverless/infra/functions/api/HttpLambdaHandler.java
+++ src/main/java/boilerplate_spring_boot_serverless/boilerplate_spring_boot_serverless/infra/functions/api/HttpLambdaHandler.java
@@ -1,40 +1,41 @@
 package boilerplate_spring_boot_serverless.boilerplate_spring_boot_serverless.infra.functions.api;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import com.amazonaws.services.lambda.runtime.Context;
+import boilerplate_spring_boot_serverless.boilerplate_spring_boot_serverless.BoilerplateSpringBootServerlessApplication;
 import com.amazonaws.serverless.exceptions.ContainerInitializationException;
 import com.amazonaws.serverless.proxy.internal.LambdaContainerHandler;
 import com.amazonaws.serverless.proxy.model.AwsProxyResponse;
 import com.amazonaws.serverless.proxy.model.HttpApiV2ProxyRequest;
 import com.amazonaws.serverless.proxy.spring.SpringBootLambdaContainerHandler;
 import com.amazonaws.serverless.proxy.spring.SpringBootProxyHandlerBuilder;
+import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler;
-import boilerplate_spring_boot_serverless.boilerplate_spring_boot_serverless.BoilerplateSpringBootServerlessApplication;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class HttpLambdaHandler implements RequestStreamHandler {
-    private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
+  private static SpringBootLambdaContainerHandler<HttpApiV2ProxyRequest, AwsProxyResponse> handler;
 
-    static {
-        try {
-            LambdaContainerHandler.getContainerConfig().setInitializationTimeout(20000);
+  static {
+    try {
+      LambdaContainerHandler.getContainerConfig().setInitializationTimeout(20000);
 
-            handler = new SpringBootProxyHandlerBuilder<HttpApiV2ProxyRequest>()
-                    .defaultHttpApiV2Proxy()
-                    .servletApplication()
-                    .springBootApplication(BoilerplateSpringBootServerlessApplication.class)
-                    .buildAndInitialize();
+      handler =
+          new SpringBootProxyHandlerBuilder<HttpApiV2ProxyRequest>()
+              .defaultHttpApiV2Proxy()
+              .servletApplication()
+              .springBootApplication(BoilerplateSpringBootServerlessApplication.class)
+              .buildAndInitialize();
 
-        } catch (ContainerInitializationException e) {
-            e.printStackTrace();
-            throw new RuntimeException("Could not initialize Spring Boot application", e);
-        }
+    } catch (ContainerInitializationException e) {
+      e.printStackTrace();
+      throw new RuntimeException("Could not initialize Spring Boot application", e);
     }
+  }
 
-    @Override
-    public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
-            throws IOException {
-        handler.proxyStream(inputStream, outputStream, context);
-    }
+  @Override
+  public void handleRequest(InputStream inputStream, OutputStream outputStream, Context context)
+      throws IOException {
+    handler.proxyStream(inputStream, outputStream, context);
+  }
 }
